d0578ba2a472888f198e74123880d0ce97cfbbfd
Janindumaleesha <janindu.maleesha@app360.lk>
2025-08-11 01:57:02 +0530

    change extention

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 import { motion } from 'framer-motion'
-import profileImage from '../assets/images/profile.jpeg'
+import profileImage from '../assets/images/profile.jpg'
 
 const Home = () => {
   return (
